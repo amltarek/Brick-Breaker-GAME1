6cd7927d52e6678b051659d7f7b433d51b4b93f8
Youssef Allam <202200286@zewailcity.edu.eg>
2023-12-14 17:55:53 +0200

    a

--- ball_paddle.cpp
+++ ball_paddle.cpp
@@ -18,7 +18,7 @@ void paddle::move_paddle_left(paddle* apaddle)
 	pGame->getWind()->SetPen(LAVENDER, 1);
 	pGame->getWind()->SetBrush(LAVENDER);
 	pGame->getWind()->DrawRectangle(uprLft.x, uprLft.y, uprLft.x + width, uprLft.y + height);
-	apaddle->uprLft.x = uprLft.x - width;
+	apaddle->uprLft.x = uprLft.x - 20;
 	apaddle->draw();
 }
 
@@ -27,7 +27,7 @@ void paddle::move_paddle_right(paddle* apaddle)
 	pGame->getWind()->SetPen(LAVENDER, 1);
 	pGame->getWind()->SetBrush(LAVENDER);
 	pGame->getWind()->DrawRectangle(uprLft.x, uprLft.y, uprLft.x + width, uprLft.y + height);
-	apaddle->uprLft.x = uprLft.x + width;
+	apaddle->uprLft.x = uprLft.x + 20;
 	apaddle->draw();
 }
 
@@ -49,17 +49,19 @@ void ball::move_ball(ball* aball, float velocity[])
 	pGame->getWind()->SetPen(LAVENDER, 1);
 	pGame->getWind()->SetBrush(LAVENDER);
 	pGame->getWind()->DrawRectangle(uprLft.x, uprLft.y, uprLft.x + width, uprLft.y + height);
-	uprLft.y = uprLft.y + velocity[1];
-	uprLft.x = uprLft.x + velocity[0];
+	uprLft.y = uprLft.y + velocity[1]*2;
+	uprLft.x = uprLft.x + velocity[0]*2;
 	aball->draw();
 }
 
 void ball::get_velocity(ball* aball, paddle* apaddle,float velocity[])
 {
 	point z=Collision_Check(aball, apaddle);
-	float pos_onpaddle= (apaddle->getWidth()/2)-z.x;
+	float pos_onpaddle= (apaddle->getWidth())-z.x;
+	float pos_from_center = pos_onpaddle - apaddle->getWidth() / 2;
 	float fraction = pos_onpaddle / (apaddle->getWidth() / 2);
 	float angle = fraction * 45;
+	float angle_rad = fraction * 3.14 / 180;
 	if (z.x!=0&&z.y!=0) {
 		velocity[0] = cos(angle);
 		velocity[1] = sin(angle);
