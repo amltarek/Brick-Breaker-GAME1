7322548a4dfe2508d44aa3f8e314ea32ce9add45
Youssef Allam <202200286@zewailcity.edu.eg>
2023-12-15 21:41:22 +0200

    aw3eq31e

--- ball_paddle.cpp
+++ ball_paddle.cpp
@@ -15,20 +15,25 @@ void paddle::collisionAction()
 /////////////////////////////////////////////////////Paddle Movement//////////////////////////////////////////////////////////////////
 void paddle::move_paddle_left(paddle* apaddle)
 {
-	pGame->getWind()->SetPen(LAVENDER, 1);
-	pGame->getWind()->SetBrush(LAVENDER);
-	pGame->getWind()->DrawRectangle(uprLft.x, uprLft.y, uprLft.x + width, uprLft.y + height);
-	apaddle->uprLft.x = uprLft.x - 20;
-	apaddle->draw();
+	if (uprLft.x - 20 >= 0)
+	{
+		pGame->getWind()->SetPen(LAVENDER, 1);
+		pGame->getWind()->SetBrush(LAVENDER);
+		pGame->getWind()->DrawRectangle(uprLft.x, uprLft.y, uprLft.x + width, uprLft.y + height);
+		apaddle->uprLft.x = uprLft.x - 20;
+		apaddle->draw();
+	}
 }
 
 void paddle::move_paddle_right(paddle* apaddle)
 {
-	pGame->getWind()->SetPen(LAVENDER, 1);
-	pGame->getWind()->SetBrush(LAVENDER);
-	pGame->getWind()->DrawRectangle(uprLft.x, uprLft.y, uprLft.x + width, uprLft.y + height);
-	apaddle->uprLft.x = uprLft.x + 20;
-	apaddle->draw();
+	if (uprLft.x + width + 20 <= pGame->getWind()->GetWidth()) {
+		pGame->getWind()->SetPen(LAVENDER, 1);
+		pGame->getWind()->SetBrush(LAVENDER);
+		pGame->getWind()->DrawRectangle(uprLft.x, uprLft.y, uprLft.x + width, uprLft.y + height);
+		apaddle->uprLft.x = uprLft.x + 20;
+		apaddle->draw();
+	}
 }
 
 
@@ -49,20 +54,42 @@ void ball::move_ball(ball* aball, float velocity[])
 	pGame->getWind()->SetPen(LAVENDER, 1);
 	pGame->getWind()->SetBrush(LAVENDER);
 	pGame->getWind()->DrawRectangle(uprLft.x, uprLft.y, uprLft.x + width, uprLft.y + height);
-	uprLft.y = uprLft.y + velocity[1]*2;
-	uprLft.x = uprLft.x + velocity[0]*2;
+	point prevPosition = uprLft;
+
+
+	uprLft.y += velocity[1] * 2;
+	uprLft.x += velocity[0] * 2;
+
+	if (uprLft.y <= 40) {
+		velocity[1] = -velocity[1];
+		uprLft.y = 40 + 1;
+	}
+	else if (uprLft.y <= 40 || uprLft.y >= pGame->getWind()->GetHeight() - height - 40) {
+		velocity[1] = -velocity[1];
+		uprLft = prevPosition;
+	}
+
+
+	if (uprLft.x <= 0 || uprLft.x >= pGame->getWind()->GetWidth() - width) {
+
+		velocity[0] = -velocity[0];
+		uprLft = prevPosition;
+
+
+	}
 	aball->draw();
+
 }
 
-void ball::get_velocity(ball* aball, paddle* apaddle,float velocity[])
+void ball::get_velocity(ball* aball, paddle* apaddle, float velocity[])
 {
-	point z=Collision_Check(aball, apaddle);
-	float pos_onpaddle= (apaddle->getWidth())-z.x;
-	float pos_from_center = pos_onpaddle - apaddle->getWidth() / 2;
+	point z = Collision_Check(aball, apaddle);
+	float pos_onpaddle = (apaddle->getWidth()) - z.x;
+	float pos_from_center = pos_onpaddle - (apaddle->getWidth() / 2);
 	float fraction = pos_onpaddle / (apaddle->getWidth() / 2);
 	float angle = fraction * 45;
 	float angle_rad = fraction * 3.14 / 180;
-	if (z.x!=0&&z.y!=0) {
+	if (z.x != 0 && z.y != 0) {
 		velocity[0] = cos(angle);
 		velocity[1] = sin(angle);
 	}
