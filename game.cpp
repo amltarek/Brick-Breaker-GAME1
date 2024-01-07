2ec345fd80bb4d6bff44da6624cfe47117247ce3
Youssef Allam <202200286@zewailcity.edu.eg>
2024-01-07 22:42:39 +0200

    aa

--- game.cpp
+++ game.cpp
@@ -309,9 +309,10 @@ void game::go() const
 						temppaddle->draw();
 						space = wait_key(moveball);
 					}
-					tempball->get_velocity();
-					tempball->brickdeflection();
-					
+					if (tempball->getPosition().y>400)
+						tempball->get_velocity();
+					else
+						tempball->brickdeflection();
 					for (int i = 0; i < currentcollect; i++) {
 						a1[i]->move_collectible();
 						point collisionpoint = collidable::Collision_Check(a1[i], temppaddle);
